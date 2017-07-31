1292f760a318d1d849e1fd81d2c26860b379b5a9
Ashish Kularia <ashishkularia@gmail.com>
2017-07-31 13:45:27 +0530

    Pizza Order Using Nested Switch Statement
    Added another class of NestedSwitch for Pizza Order

--- java/july/assignment.java
+++ java/july/assignment.java
@@ -90,3 +90,172 @@ class Assignment_2_31_07_2017{
 		System.out.println("Totol bill : " + bill);
 	}
 }
+
+class Assignment_3_31_07_2017{
+	int type;
+	String top, size;
+	public static void main(String[] args) {
+		Assignment_3_31_07_2017 obj=new Assignment_3_31_07_2017();
+		obj.function_3();
+	}
+	void function_3() {
+		Scanner obj=new Scanner(System.in);
+		System.out.println("Enter 1 for veg and 2 for Non-veg");
+		System.out.println("");
+		type=obj.nextInt();
+		switch (type) {
+			case 1:
+				System.out.println("Enter the topping from following");
+				System.out.println("on for Onion");
+				System.out.println("cap for Capsicum");
+				System.out.println("sc for Sweet Corn");
+				System.out.println("");
+				top=obj.next();
+				switch (top) {
+					case "on":
+						System.out.println("Select the Size");
+						System.out.println("Sm for Small");
+						System.out.println("Med for Medium");
+						System.out.println("Lg for Large");
+						System.out.println("");
+						size = obj.next();
+						switch (size) {
+							case "sm":
+								System.out.println("Kindly review your order \"Veg. Onion Small Pizza\"");
+								break;
+							case "med":
+								System.out.println("Kindly review your order \"Veg. Onion Medium Pizza\"");
+								break;
+							case "lg":
+								System.out.println("Kindly review your order \"Veg. Onion Large Pizza\"");
+								break;
+							default:
+								System.out.println("Invalid Input");
+						}
+						break;
+					case "cap":
+						System.out.println("Select the Size");
+						System.out.println("Sm for Small");
+						System.out.println("Med for Medium");
+						System.out.println("Lg for Large");
+						System.out.println("");
+						size = obj.next();
+						switch (size) {
+							case "sm":
+								System.out.println("Kindly review your order \"Veg. Capsicum Small Pizza\"");
+								break;
+							case "med":
+								System.out.println("Kindly review your order \"Veg. Capsicum Medium Pizza\"");
+								break;
+							case "lg":
+								System.out.println("Kindly review your order \"Veg. Capsicum Large Pizza\"");
+								break;
+							default:
+								System.out.println("Invalid Input");
+						}
+						break;
+					case "sc":
+						System.out.println("Select the Size");
+						System.out.println("Sm for Small");
+						System.out.println("Med for Medium");
+						System.out.println("Lg for Large");
+						System.out.println("");
+						size = obj.next();
+						switch (size) {
+							case "sm":
+								System.out.println("Kindly review your order \"Veg. Sweet Corn Small Pizza\"");
+								break;
+							case "med":
+								System.out.println("Kindly review your order \"Veg. Sweet Corn Medium Pizza\"");
+								break;
+							case "lg":
+								System.out.println("Kindly review your order \"Veg. Sweet Corn Large Pizza\"");
+								break;
+							default:
+								System.out.println("Invalid Input");
+						}
+						break;
+					default:
+						System.out.println("Invalid Input");
+				}
+				break;
+			case 2:
+				System.out.println("Enter the topping from following");
+				System.out.println("hc for Hot and spicy Chicken");
+				System.out.println("cs for Chicken Salami");
+				System.out.println("cc for Chunky Chicken");
+				System.out.println("");
+				top=obj.next();
+				switch (top) {
+					case "hc":
+						System.out.println("Select the Size");
+						System.out.println("Sm for Small");
+						System.out.println("Med for Medium");
+						System.out.println("Lg for Large");
+						System.out.println("");
+						size = obj.next();
+						switch (size) {
+							case "sm":
+								System.out.println("Kindly review your order \"Non-Veg Hot and Spicy Chicken Small Pizza\"");
+								break;
+							case "med":
+								System.out.println("Kindly review your order \"Non-Veg Hot and Spicy Chicken Medium Pizza\"");
+								break;
+							case "lg":
+								System.out.println("Kindly review your order \"Non-Veg Hot and Spicy Chicken Large Pizza\"");
+								break;
+							default:
+								System.out.println("Invalid Input");
+						}
+						break;
+					case "cs":
+						System.out.println("Select the Size");
+						System.out.println("Sm for Small");
+						System.out.println("Med for Medium");
+						System.out.println("Lg for Large");
+						System.out.println("");
+						size = obj.next();
+						switch (size) {
+							case "sm":
+								System.out.println("Kindly review your order \"Non-veg Chicken Salami Small Pizza\"");
+								break;
+							case "med":
+								System.out.println("Kindly review your order \"Non-veg Chicken Salami Medium Pizza\"");
+								break;
+							case "lg":
+								System.out.println("Kindly review your order \"Non-veg Chicken Salami Large Pizza\"");
+								break;
+							default:
+								System.out.println("Invalid Input");
+						}
+						break;
+					case "cc":
+						System.out.println("Select the Size");
+						System.out.println("Sm for Small");
+						System.out.println("Med for Medium");
+						System.out.println("Lg for Large");
+						System.out.println("");
+						size = obj.next();
+						switch (size) {
+							case "sm":
+								System.out.println("Kindly review your order \"Non-veg Chunky Chicken Small Pizza\"");
+								break;
+							case "med":
+								System.out.println("Kindly review your order \"Non-veg Chunky Chicken Medium Pizza\"");
+								break;
+							case "lg":
+								System.out.println("Kindly review your order \"Non-veg Chunky Chicken Large Pizza\"");
+								break;
+							default:
+								System.out.println("Invalid Input");
+						}
+						break;
+					default:
+						System.out.println("Invalid Input");
+				}
+				break;
+			default:
+				System.out.println("Invalid Input");
+		}
+	}
+}
