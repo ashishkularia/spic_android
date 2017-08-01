6f059c415c665e3b174eb68fab4cf8ae5d6be0b5
Ashish Kularia <ashishkularia@gmail.com>
2017-08-01 14:46:55 +0530

    Removed minnor bugs

--- java/aug/1.java
+++ java/aug/1.java
@@ -7,89 +7,106 @@ class WhileLoop {
 		double amt=0.0, camt=0.0, deduction=0.0;
 		Scanner sc=new Scanner(System.in);
 		do {
-			System.out.println("Do you want to Book or Cancel?");
+			System.out.println("\nDo you want to Book or Cancel?");
 			btype = sc.next();
 			switch (btype) {
 				case "Book":
 				case "book":
 				case "BOOK":
-					System.out.println("Enter Class type");
-					System.out.println(" Gold 300 \n Silver 200 \n Normal 100 \n");
+					System.out.println("\nEnter Class type");
+					System.out.println("\nGold costs Rs.300 \nSilver costs Rs.200 \nNormal costs Rs.100");
 					type=sc.next();
-					System.out.println("Enter number of seats");
+					System.out.println("\nEnter number of seats");
 					seats=sc.nextInt();
 					switch (type){
 						case "Gold":
 						case "gold":
 						case "GOLD":
 							amt=seats*300;
-							System.out.println("\nYour Bill is "+amt+"\n");
+							System.out.println("\nYour Bill is "+amt);
 							break;
 						case "Normal":
 						case "normal":
 						case "NORMAL":
 							amt=seats*100;
-							System.out.println("Your Bill is "+amt+"\n");
+							System.out.println("\nYour Bill is "+amt);
 							break;
 						case "Silver":
 						case "silver":
 						case "SILVER":
 							amt=seats*200;
-							System.out.println("Your Bill is "+amt+"\n");
+							System.out.println("\nYour Bill is "+amt);
 							break;
 						default:
-							System.out.println("Invalid Choice");
+							System.out.println("\nInvalid Choice");
 						}
 					break;
 
 				case "Cancel":
 				case "cancel":
 				case "CANCEL":
-					System.out.println("Enter Class type");
-					System.out.println(" Gold 300 \n Silver 200 \n Normal 100 \n");
-					ctype=sc.next();
-					System.out.println("Enter the number of seats to cancel");
-					cseats=sc.nextInt();
-					if (ctype.equals(type)) {
-						if (cseats<=seats) {
-							switch (type){
-								case "Gold":
-								case "gold":
-								case "GOLD":
-									camt=seats*300;
-									deduction=camt*25/100;
-									System.out.println("\nYour Bill is "+deduction+"\n");
-									break;
-								case "Normal":
-								case "normal":
-								case "NORMAL":
-									camt=seats*100;
-									deduction=camt*25/100;
-									System.out.println("Your Bill is "+deduction+"\n");
-									break;
-								case "Silver":
-								case "silver":
-									case "SILVER":
-									camt=seats*200;
-									deduction=camt*25/100;
-									System.out.println("Your Bill is "+deduction+"\n");
-									break;
-								default:
-									System.out.println("Invalid Choice");
+					if (type.equals("")) {
+						System.out.println("\nSorry Cancellation cannot be proceeded as their is no booking details available.");
+					}
+					else	{
+						System.out.println("\nEnter Class type you want to cancel.");
+						System.out.println("\nGold\nSilver\nNormal");
+						ctype=sc.next();
+						System.out.println("\nEnter the number of seats to cancel");
+						cseats=sc.nextInt();
+						if ((type.equals("gold")||type.equals("Gold")||type.equals("GOLD"))&&(ctype.equals("gold")||ctype.equals("Gold")||ctype.equals("GOLD"))) {ctype=type;}
+						if ((type.equals("silver")||type.equals("Silver")||type.equals("SILVER"))&&(ctype.equals("silver")||ctype.equals("Silver")||ctype.equals("SILVER"))) {ctype=type;}
+						if ((type.equals("normal")||type.equals("Normal")||type.equals("NORMAL"))&&(ctype.equals("normal")||ctype.equals("Normal")||ctype.equals("NORMAL"))) {ctype=type;}
+						if (ctype.equals(type)) {
+							if (cseats<=seats) {
+								switch (type){
+									case "Gold":
+									case "gold":
+									case "GOLD":
+										camt=seats*300;
+										System.out.println("Your Bill was of Rs. " + amt);
+										deduction=camt*25/100;
+										System.out.println("\n Cancellation Charges are " + deduction);
+										camt -= deduction;
+										System.out.println("\nAmount that will returned is "+camt+"\n");
+										break;
+									case "Normal":
+									case "normal":
+									case "NORMAL":
+										camt=seats*100;
+										System.out.println("Your Bill was of Rs. " + amt);
+										deduction=camt*25/100;
+										System.out.println("\n Cancellation Charges are " + deduction);
+										camt -= deduction;
+										System.out.println("\nAmount that will returned is "+camt+"\n");
+										break;
+									case "Silver":
+									case "silver":
+										case "SILVER":
+										camt=seats*200;
+										System.out.println("Your Bill was of Rs. " + amt);
+										deduction=camt*25/100;
+										System.out.println("\n Cancellation Charges are " + deduction);
+										camt -= deduction;
+										System.out.println("\nAmount that will returned is "+camt+"\n");
+										break;
+									default:
+										System.out.println("Invalid Choice");
 								}
+							}
+							else{
+								System.out.println("your Booked tickets are " + seats + ".\nSo, kindly enter the correct number of seats.");
+							}
 						}
 						else{
-							System.out.println("your Booked tickets are " + seats + ".\nSo, kindly enter the correct number of seats.");
+							System.out.println("\nKindle enter your booking class.");
 						}
 					}
-					else{
-						System.out.println("Kindle book First or Enter the same class.");
-					}
 					break;
-				}
-
-			
-			System.out.println("Do you want to continue booking tickets? Yes or No \n");
+				default:
+					System.out.println("Kindly say \"Yes\" or \"No\"");
+			}
+			System.out.println("\nDo you want to do another booking or Cancellation? Yes or No");
 			choice = sc.next();
 		}
 		while (choice.equals("Yes")||choice.equals("yes")||choice.equals("YES"));
