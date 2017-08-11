cd1c583b52d95caad7d4b143820646fa18e59e2b
Ashish Kularia <ashishkularia@gmail.com>
2017-08-11 13:00:15 +0530

    Added interface and event handler class
    EDM is consist of Source class, interfaces, EventClasses.
    
    All graphichal items like button, textField, checkbox are source class
    byt heir name respectively.
    
    Each source class have their specific event handling classes. and those
    classes can be access by Interface
    
    Event handling classes are ActionEvent, and more.
    
    Interface is ActionListener
    
    First of all we need to register the object of source class to the
    interface. only then that particular event will occur.

--- java/aug/First_11.java
+++ java/aug/First_11.java
@@ -1,20 +1,48 @@
 import java.awt.*;
 import java.applet.*;
+import java.awt.event.*;
 
 /*
 	<applet code="First_11.class" height=400 width=400></applet>
 */
 
-public class First_11 extends Applet {
-	Button login;
+public class First_11 extends Applet implements ActionListener {
+	Button login, red, blue, green;
 	TextField txt;
 	public void init() {
 		System.out.println("This is inti()");
 		Color c=new Color(125,025,200);
 		setBackground(c);
 		login = new Button("Login");
+		red= new Button("Red");
+		green = new Button("Green");
+		blue = new Button("Blue");
 		add(login);
+		add(red);
+		add(green);
+		add(blue);
+		login.addActionListener(this);
+		red.addActionListener(this);
+		green.addActionListener(this);
+		blue.addActionListener(this);
 	}
+
+	public void actionPerformed(ActionEvent ae) {
+		String str= ae.getActionCommand();
+		System.out.println(""+str);
+		Object obj = ae.getSource();
+		System.out.println("Source is "+obj);
+		if (str.equals("Red")) {
+			setBackground(Color.red);
+		}
+		if (str.equals("Blue")) {
+			setBackground(Color.blue);
+		}
+		if (str.equals("Green")) {
+			setBackground(Color.green);
+		}
+	}
+
 	public void start() {
 		System.out.println("This is start()");
 	}
